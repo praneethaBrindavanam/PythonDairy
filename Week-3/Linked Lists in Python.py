912d9301367687b11de31616dd2368d428ee1005
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2024-10-29 21:31:55 +0530

    Create Linked Lists in Python.py

---
+++ Week-3/Linked Lists in Python.py
@@ -0,0 +1,9 @@
+Linked Lists in Python
+A Linked List is a linear data structure where elements (called nodes) are stored in a sequence, but not in contiguous memory locations. Each node contains:
+
+Data: The actual value or data of the node.
+Pointer: A reference (or link) to the next node in the list.
+There are two main types of linked lists:
+
+Singly Linked List
+Doubly Linked List
